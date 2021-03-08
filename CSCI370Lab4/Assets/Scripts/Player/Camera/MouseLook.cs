facc57544957b6f3ee50a038d54bb49e0c7439e8
Mitchell <mlrotenberry99@yahoo.com>
2021-03-07 19:27:52 -0600

    Added playermovement

--- CSCI370Lab4/Assets/Scripts/Player/Camera/MouseLook.cs
+++ CSCI370Lab4/Assets/Scripts/Player/Camera/MouseLook.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
 
-    public float MouseSensitivity = 100f;
+    public float MouseSensitivity = 1000f;
 
-    float xRotation = 0f;
     public Transform playerBody;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,16 +16,11 @@ public class MouseLook : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity;
 
-        /*xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);*/
         playerBody.Rotate(Vector3.up * mouseX);
         playerBody.Rotate(Vector3.right * mouseY);
     }
