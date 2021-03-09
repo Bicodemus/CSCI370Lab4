e02b5fe76bdaec0473fec58cdef729ca274737e5
Morgan McLeod <morganmcleod1123@gmail.com>
2021-03-09 11:23:38 -0600

    Set up skeleton for main menu. Functional but not pretty

--- CSCI370Lab4/Assets/Scripts/GameManager.cs
+++ CSCI370Lab4/Assets/Scripts/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     public int interactionCount = 3;
+    public int sceneToLoad;
 
 
     private void Awake()
@@ -29,5 +31,10 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void nextScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+        sceneToLoad++;
     }
 }
---
+++ CSCI370Lab4/Assets/Scripts/Interactable.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public BoxCollider triggerBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player Hit");
+            GameManager.Instance.interactionCount--;
+            triggerBox.size = new Vector3(0, 0, 0);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("HIT!");
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Leaving");
+    }
+}
