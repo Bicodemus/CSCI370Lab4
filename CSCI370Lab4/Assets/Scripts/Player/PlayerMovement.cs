675d7e6271d1368c59358b57db3aa46b68a8fda2
Mitchell <mlrotenberry99@yahoo.com>
2021-03-10 17:57:04 -0600

    Changed camera functionality

--- CSCI370Lab4/Assets/Scripts/Player/PlayerMovement.cs
+++ CSCI370Lab4/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,22 +6,35 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController controller;
-
+    public Transform cam;
     public float speed = 12f;
+
+    public float turnTime = 0.1f;
+    float turnSmoothVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
+        Vector3 move = new Vector3(x, 0f, z).normalized;
+
+        if (move.magnitude >= 0.1f)
+        {
+            float targetAng = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAng, ref turnSmoothVelocity, turnTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAng, 0f) * Vector3.forward; 
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
     }
 }
