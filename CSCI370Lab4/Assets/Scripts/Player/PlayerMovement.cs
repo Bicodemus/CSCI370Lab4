66b34ba059a5524221e2c3c506d264075184c76c
Mitchell <mlrotenberry99@yahoo.com>
2021-03-10 18:45:35 -0600

    Added movement on the Y axis

--- CSCI370Lab4/Assets/Scripts/Player/PlayerMovement.cs
+++ CSCI370Lab4/Assets/Scripts/Player/PlayerMovement.cs
@@ -23,7 +23,9 @@ public class PlayerMovement : MonoBehaviour
     {
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
+        float up = Input.GetAxisRaw("Up");
 
+        Vector3 upDown = new Vector3(0f, up, 0f);
         Vector3 move = new Vector3(x, 0f, z).normalized;
 
         if (move.magnitude >= 0.1f)
@@ -32,9 +34,17 @@ public class PlayerMovement : MonoBehaviour
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAng, ref turnSmoothVelocity, turnTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
+            /*float targetAng2 = Mathf.Atan2(move.z, move.y) * Mathf.Rad2Deg + cam.eulerAngles.x;
+            float angle2 = Mathf.SmoothDampAngle(transform.eulerAngles.x, targetAng2, ref turnSmoothVelocity, turnTime);
+            transform.rotation = Quaternion.Euler(angle2, angle, 0f);
+
+
+            Vector3 moveDirection = Quaternion.Euler(targetAng2, targetAng, 0f) * Vector3.forward;*/
 
             Vector3 moveDirection = Quaternion.Euler(0f, targetAng, 0f) * Vector3.forward; 
             controller.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
+
+        controller.Move(upDown * speed * Time.deltaTime);
     }
 }
