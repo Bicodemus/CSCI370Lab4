c9df2b70b54cea7a7efb81132eff50620b247d03
Mitchell <mlrotenberry99@yahoo.com>
2021-03-12 00:09:51 -0600

    got more pngs

--- CSCI370Lab4/Assets/Scripts/Player/Camera/MouseLook.cs
+++ CSCI370Lab4/Assets/Scripts/Player/Camera/MouseLook.cs
@@ -9,15 +9,21 @@ public class MouseLook : MonoBehaviour
 
     public Transform playerBody;
 
+    private Camera myCam;
+
     // Start is called before the first frame update
     void Start()
     {
+        myCam = GetComponent<Camera>();
+
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        myCam.orthographicSize = (Screen.height / 100f);
+
         float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
         /*float z = Input.GetAxis("Horizontal") * MouseSensitivity * Time.deltaTime;
